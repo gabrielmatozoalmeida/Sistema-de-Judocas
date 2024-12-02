6650f96600103ecffcd2a70070b287c2900d950a
gabrielmatozoalmeida <127260693+gabrielmatozoalmeida@users.noreply.github.com>
2024-12-02 11:50:45 -0300

    DaoErroMain.java

--- src/org/fpij/jitakyoei/util/Main.java
+++ src/org/fpij/jitakyoei/util/Main.java
@@ -17,55 +17,92 @@ import org.fpij.jitakyoei.view.AppView;
 import org.fpij.jitakyoei.view.MainAppView;
 
 public class Main {
-	
-	public static void main(String[] args) {
-//		new StartupHelper().initialize();
-//		PlafOptions.setAsLookAndFeel();
-//		dbPopulator();
-		
-		try {
-		    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-		        if ("Nimbus".equals(info.getName())) {
-		            UIManager.setLookAndFeel(info.getClassName());
-		            break;
-		        }
-		    }
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		AppView view = new MainAppView();
-		AppFacade facade = new AppFacadeImpl(view);
-		view.registerFacade(facade);
-	}
-	
-	public static void dbPopulator(){
-		Endereco endereco = new Endereco();
-		endereco.setBairro("Dirceu");
-		endereco.setCep("64078-213");
-		endereco.setCidade("Teresina");
-		endereco.setEstado("PI");
-		endereco.setRua("Rua Des. Berilo Mota");
-		
-		Filiado filiadoProf = new Filiado();
-		filiadoProf.setNome("Neto");
-		filiadoProf.setCpf("036.464.453-27");
-		filiadoProf.setDataNascimento(new Date());
-		filiadoProf.setDataCadastro(new Date());
-		filiadoProf.setId(3332L);
-		filiadoProf.setEndereco(endereco);
-		
-		Professor professor = new Professor();
-		professor.setFiliado(filiadoProf);
-		
-		Entidade entidade = new Entidade();
-		entidade.setEndereco(endereco);
-		entidade.setNome("Ricardo Paraguasu");
-		entidade.setTelefone1("(086)1234-5432");
-		
-		DAO<Professor> dao = new DAOImpl<Professor>(Professor.class);
-		dao.save(professor);
-		
-		DAO<Entidade> daoEnt = new DAOImpl<Entidade>(Entidade.class);
-		daoEnt.save(entidade);
-	}
+
+    public static void main(String[] args) {
+        // Configuração do LookAndFeel
+        try {
+            for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            System.err.println("Erro ao configurar LookAndFeel: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        // Inicialização da aplicação
+        AppView view = new MainAppView();
+        AppFacade facade = new AppFacadeImpl(view);
+        view.registerFacade(facade);
+
+        // Populando banco de dados com dados de exemplo
+        dbPopulator();
+    }
+
+    public static void dbPopulator() {
+        // Configuração de endereço com validação
+        Endereco endereco = new Endereco();
+        String cep = "64078-213";
+        if (cep.matches("\\d{5}-\\d{3}")) { // Validação de formato do CEP (XXXXX-XXX)
+            endereco.setCep(cep);
+        } else {
+            System.err.println("CEP inválido: " + cep);
+            return;
+        }
+
+        endereco.setBairro("Dirceu");
+        endereco.setCidade("Teresina");
+        endereco.setEstado("PI");
+        endereco.setRua("Rua Des. Berilo Mota");
+
+        // Configuração de filiado (Professor) com validação de CPF
+        Filiado filiadoProf = new Filiado();
+        String cpf = "036.464.453-27";
+        if (cpf.matches("\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}")) { // Validação de formato do CPF
+            filiadoProf.setCpf(cpf);
+        } else {
+            System.err.println("CPF inválido: " + cpf);
+            return;
+        }
+
+        filiadoProf.setNome("Neto");
+        filiadoProf.setDataNascimento(new Date());
+        filiadoProf.setDataCadastro(new Date());
+        filiadoProf.setId(3332L);
+        filiadoProf.setEndereco(endereco);
+
+        Professor professor = new Professor();
+        professor.setFiliado(filiadoProf);
+
+        // Configuração de entidade
+        Entidade entidade = new Entidade();
+        entidade.setEndereco(endereco);
+        entidade.setNome("Ricardo Paraguasu");
+        entidade.setTelefone1("(086)1234-5432");
+
+        // Persistência dos dados no banco com tratamento de exceções
+        persistData(professor, entidade);
+    }
+
+    private static void persistData(Professor professor, Entidade entidade) {
+        try {
+            DAO<Professor> professorDao = new DAOImpl<>(Professor.class);
+            professorDao.save(professor);
+            System.out.println("Professor salvo com sucesso no banco de dados.");
+        } catch (Exception e) {
+            System.err.println("Erro ao salvar professor no banco de dados: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        try {
+            DAO<Entidade> entidadeDao = new DAOImpl<>(Entidade.class);
+            entidadeDao.save(entidade);
+            System.out.println("Entidade salva com sucesso no banco de dados.");
+        } catch (Exception e) {
+            System.err.println("Erro ao salvar entidade no banco de dados: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
